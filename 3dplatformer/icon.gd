227ce368ef3cc98eae6ccf2c89679361ba62fb97
V102002 <vssnsvs@gmail.com>
2024-10-15 13:25:51 +0530

    Added an if and elif statement

--- 3dplatformer/icon.gd
+++ 3dplatformer/icon.gd
@@ -3,6 +3,7 @@ extends Sprite3D
 var coins = 5 #Number
 var player_name = "Robot" #String
 var hearts = 3.5 #float
+var key_collected=true #Boolean
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -10,4 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate_y(0.1)
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-2))
+	if Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(2))

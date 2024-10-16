c0b7ff565384eae39321a0d139e5322660c219f2
V102002 <vssnsvs@gmail.com>
2024-10-16 14:33:38 +0530

    Added coin model

--- 3dplatformer/coin.gd
+++ 3dplatformer/coin.gd
@@ -1,6 +1,6 @@
 extends Area3D
-
-
+#how fast the coin should rotate
+const ROT_SPEED = 2 #Number of degrees the coin rotates every frame
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rotate_y(deg_to_rad(ROT_SPEED))
+	
+	#Shouldn't use has_overlapping_bodies() because there will be a slight delay between detection of collision and reaction to the collision. 
+	#Use Signals- Signals are a way of handling common types of events
+	#if has_overlapping_bodies(): #If the coin is colliding or overlapping with the any other physics object
+		#queue_free() #Deletes the object from the game
+
+func _on_body_entered(body: Node3D) -> void:
+	queue_free() #Deletes the object from the game

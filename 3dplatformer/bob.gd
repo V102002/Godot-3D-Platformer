dc3959922a0047e514144997f36fce896f5eee54
V102002 <vssnsvs@gmail.com>
2024-10-16 20:42:37 +0530

    Camera Smoothening

--- 3dplatformer/bob.gd
+++ 3dplatformer/bob.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -26,3 +27,10 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	#Make Camera Controller Match the position of Bob
+	$Camera_Controller.position=lerp($Camera_Controller.position,position,0.12)
+	
+	
+	
+	
+	
